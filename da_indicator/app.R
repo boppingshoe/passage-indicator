d8c5c317797a8f1830abc9347708a162a24e6a76
boppingshoe <boppingshoester@gmail.com>
2018-02-13 16:51:39 -0800

    adding options for random year effects and number of simulations.

--- da_indicator/app.R
+++ da_indicator/app.R
@@ -12,12 +12,11 @@ library(arm)
 load(file= 'data/adop.Rdata')
 load(file= 'data/ad_dat.Rdata')
 
-
 # Define UI for application that... ----
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Benner Indicator"),
+   titlePanel("LGS Passage Indicator"),
    
    # Sidebar with a slider input for which year to predict
   sidebarLayout(
@@ -26,7 +25,10 @@ ui <- fluidPage(
         label = "Select a year to display", 
         choices = list("2009" = 2009, "2010" = 2010, "2011" = 2011, "2012" = 2012,
           "2013" = 2013, "2014" = 2014, "2015" = 2015, "2016" = 2016, "2017" = 2017),
-        selected = 2009)),
+        selected = 2009),
+      numericInput("nsim", "Number of simulations", 
+        value = 100, min = 100, max = 500, step = 100),
+      checkboxInput("raneff", "Random year effects", value = TRUE)),
 
       # Show a plot of expected passage
       mainPanel(
@@ -35,34 +37,47 @@ ui <- fluidPage(
    )
 )
 
-# Define server logic required to... -----
+# Define server logic required to display a plot for expected counts -----
 server <- function(input, output) {
   
-  # yr_out<- reactive({
-  #     yr_set<- input$year
-  #     return(yr_set)
-  # })
-
-    
   output$passage_plot <- renderPlot({
     da_yr<- input$year
-    #da_yr<- yr_out()$yr_set
-    mdl1<- lm(log(lgs)~ log(lmn_1), data= subset(adop, lmn_1>100, obs_yr!=da_yr))
-    sim1<- sim(mdl1, n.sims=500)
-    colnames(sim1@coef)
+    n_sim<- input$nsim
+    ran<- input$raneff
+    # fitting a linear regression model and simulate predicted coefs
+    if (ran==TRUE) {
+      mdl1<- lmer(log(lgs)~ log(lmn_1)+ (1|obs_yr), data= subset(adop, lmn_1>100 & obs_yr!=da_yr))
+    } else {
+      mdl1<- lm(log(lgs)~ log(lmn_1), data= subset(adop, lmn_1>100, obs_yr!=da_yr))
+    }
     
+    sim1<- sim(mdl1, n.sims=n_sim)
+
+    # plotting fish counts
     with(ad_dat[ad_dat$obs_yr==da_yr,], 
-       plot(obs_date, cumsum(lgs), pch=20, main=da_yr, ylim=c(0, sum(lgs)+10000),
-         xlab=NA, ylab='Cumulative Counts at LGS', ty='n'))
-     for(i in 1:500){
-       with(subset(ad_dat, obs_yr==da_yr & lmn_1>0),
-         lines(obs_date, cumsum(exp(sim1@coef[i, 1]+ sim1@coef[i, 2]*log(lmn_1)) ),
-           col='grey50'))
-     }
-     # with(ad_dat[ad_dat$obs_yr==da_yr,],
-     #   lines(obs_date, cumsum(lmn_1), pch=20, lwd=2, col='blue'))
-     with(ad_dat[ad_dat$obs_yr==da_yr,],
-       lines(obs_date, cumsum(lgs), pch=20, lwd=2, col='red'))
+      plot(obs_date, cumsum(lgs), pch=20, main=da_yr, ylim=c(0, sum(lgs)+10000),
+        xlab=NA, ylab='Cumulative Counts at LGS', ty='n'))
+    # plot predicted lgs counts as uncertainties
+    for(i in 1:n_sim){
+      if (ran==TRUE) {
+        with(subset(ad_dat, obs_yr==da_yr & lmn_1>0),
+          lines(obs_date, cumsum(exp(sim1@fixef[i, 1]+ sim1@fixef[i, 2]*log(lmn_1)) ),
+            col='grey50'))
+      } else{
+        with(subset(ad_dat, obs_yr==da_yr & lmn_1>0),
+          lines(obs_date, cumsum(exp(sim1@coef[i, 1]+ sim1@coef[i, 2]*log(lmn_1)) ),
+            col='grey50'))
+      }
+    }
+
+         with(subset(ad_dat, obs_yr==da_yr), # observed lmn counts
+       lines(obs_date, cumsum(lmn_1), pch=20, lwd=3, col='blue'))
+     with(subset(ad_dat, obs_yr==da_yr), # observed lgs counts
+       lines(obs_date, cumsum(lgs), pch=20, lwd=3, col='red'))
+     with(subset(ad_dat, obs_yr==da_yr),
+       legend(min(obs_date), sum(lgs),
+         c('LMN counts','LGS counts','Expected LGS counts'), lty=1, lwd=c(3,3,10),
+         col=c('blue','red','grey50'), bty='n'))
    })
 }
 
